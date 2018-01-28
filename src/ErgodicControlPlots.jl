6a7dc64a7011c504f617ad265fa30c3778c820fa
dressel <louisdressel@gmail.com>
2018-01-28 11:24:12 -0800

    first commit

--- src/ErgodicControlPlots.jl
+++ src/ErgodicControlPlots.jl
@@ -1,5 +1,19 @@
 module ErgodicControlPlots
 
+const T2F = NTuple{2, Float64}    # x, y
+const MF = Matrix{Float64}
+const VMF = Vector{MF}
+const VF = Vector{Float64}
+const VVF = Vector{VF}
+const VVVF = Vector{VVF}
+
 # package code goes here
+using ErgodicControl
+
+const VTM = Vector{TrajectoryManager}
+
+
+include("plots.jl")
+include("gif.jl")
 
 end # module
